681f5b703081a6a3a16998235603e00efd7b6318
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:34:57 -0300

    Delete exercicio11.py

--- exercicio11.py
+++
@@ -1,4 +0,0 @@
-anos = int(input("Digite a idade em anos: "))
-meses = int(input("Digite a idade em meses: "))
-dias = int(input("Digite a idade em dias: "))
-print(f"A idade da pessoa em dias é igual a {(anos * 365) + (meses * 30) + dias} dias.")
