68b494f8871c791616718682a5b496ef8b722c47
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:35:04 -0300

    Delete exercicio12.py

--- exercicio12.py
+++
@@ -1,7 +0,0 @@
-eleitores = int(input("Digite o número total de eleitores: "))
-brancos = int(input("Digite o número de votos brancos: "))
-nulos = int(input("Digite o número de votos nulos: "))
-validos = int(input("Digite o número de votos válidos: "))
-print(f"Votos brancos: {(brancos / eleitores) * 100}%\n"
-      f"Votos nulos: {(nulos / eleitores) * 100}%\n"
-      f"Votos válidos: {(validos / eleitores) * 100}%\n")
