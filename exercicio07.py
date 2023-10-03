ba4d4eff1cf4c7b2547b65f42946d49eaa1acac5
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:34:30 -0300

    Delete exercicio07.py

--- exercicio07.py
+++
@@ -1,3 +0,0 @@
-base = float(input("Digite o número da base do triângulo: "))
-altura = float(input("Digite o número da altura do triângulo: "))
-print(f"A área do triângulo é igual a {(base * altura) / 2}")
