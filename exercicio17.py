e3b6fdc95ded109182a18144eaa80364a543947e
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:35:37 -0300

    Delete exercicio17.py

--- exercicio17.py
+++
@@ -1,5 +0,0 @@
-quantidade_macas = int(input("Digite o número de maçãs: "))
-if quantidade_macas < 12:
-    print(f"O custo total da compra é de R$ {quantidade_macas * 1.30}")
-else:
-    print(f"O custo total da compra é de R$ {quantidade_macas * 1.00}")
