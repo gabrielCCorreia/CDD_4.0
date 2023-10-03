026d76d983c45fe80c70909d6fb7750c37e20e06
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:35:10 -0300

    Delete exercicio13.py

--- exercicio13.py
+++
@@ -1,3 +0,0 @@
-base = float(input("Digite o número da base do retângulo: "))
-altura = float(input("Digite o número da altura do retângulo: "))
-print(f"A área do retângulo é igual a {base * altura}")
