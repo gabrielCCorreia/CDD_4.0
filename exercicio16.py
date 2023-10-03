904549d2c49d15faf64e9c010d91dbfa96bc1496
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:35:31 -0300

    Delete exercicio16.py

--- exercicio16.py
+++
@@ -1,7 +0,0 @@
-hora_inicio = int(input("Digite a hora de início do jogo: "))
-hora_fim = int(input("Digite a hora de fim do jogo: "))
-if hora_inicio <= hora_fim:
-    duracao = hora_fim - hora_inicio
-else:
-    duracao = (24 - hora_inicio) + hora_fim
-print(f"A duração do jogo de xadrez foi de {duracao} horas.")
