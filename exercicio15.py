7c9aff57478bec8313331ad28a42f28cdc166082
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-03 20:35:24 -0300

    Delete exercicio15.py

--- exercicio15.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Digite o 1° número inteiro: "))
-num2 = int(input("Digite o 2° número inteiro (diferente do primeiro): "))
-while num2 == num1:
-    num2 = int(input("Erro! Digite um número diferente do primeiro: "))
-if num1 < num2:
-    print(f"{num1} e {num2}")
-else:
-    print(f"{num2} e {num1}")
