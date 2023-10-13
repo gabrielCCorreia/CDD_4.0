4a365fc86494f1a3fee571baa8c6b8bde219124a
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-13 15:52:53 -0300

    Update biblioteca.py

--- 3. Projeto 2 - Jogo da Forca/biblioteca.py
+++ 3. Projeto 2 - Jogo da Forca/biblioteca.py
@@ -4,3 +4,54 @@ import random
 def escolher_palavra():
     palavras = ["python", "programacao", "computador", "inteligencia", "artificial", "desenvolvimento"]
     return random.choice(palavras)
+
+
+def bonequinho_forca(arg1):
+    print(" _________    ")
+    print(" |/      |    ")
+
+    if arg1 == 1:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if arg1 == 2:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if arg1 == 3:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if arg1 == 4:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if arg1 == 5:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if arg1 == 6:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if arg1 == 7:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print(" |__          ")
+    print()
