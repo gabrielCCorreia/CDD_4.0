a4d00dc6d3bb0162ceb100e9f8d7fb26f46794c0
gabrielCCorreia <146298406+gabrielCCorreia@users.noreply.github.com>
2023-10-28 12:10:27 -0300

    Update exercicio02.py

--- 4. Atividades sobre classes/exercicio02.py
+++ 4. Atividades sobre classes/exercicio02.py
@@ -1,61 +1,83 @@
 class ContaBancaria:
-    def __init__(self, numero_conta, tipo_conta, saldo, status_conta, nome_cliente):
+    def __init__(self, nome_cliente, numero_conta, tipo_conta):
+        self.nome_cliente = nome_cliente
         self.numero_conta = numero_conta
         self.tipo_conta = tipo_conta
-        self.saldo = saldo
-        self.status_conta = status_conta
-        self.nome_cliente = nome_cliente
+        self.saldo = 0.0
+        self.limite = 1000
+        self.limite_disponivel = self.limite
+        self.status_conta = False
 
     def depositar(self, deposito):
         if self.status_conta:
             if deposito > 0:
-                self.saldo += deposito
-                print(f"Depósito de R$ {deposito} realizado com sucesso.")
-            elif deposito == 0:
-                print(f"Erro: Digite um valor maior do que zero para depositar.")
+                if self.limite_disponivel < self.limite:
+                    self.limite_disponivel += deposito
+                    if not self.limite_disponivel <= self.limite:
+                        self.saldo += (self.limite_disponivel - self.limite)
+                        self.limite_disponivel -= (self.limite_disponivel - self.limite)
+                    print(f"Depósito de R$ {deposito:.2f} realizado com sucesso.")
+                else:
+                    self.saldo += deposito
+                    print(f"Depósito de R$ {deposito:.2f} realizado com sucesso.")
             else:
-                print(f"Erro: Não é possível realizar um depósito de valor negativo.")
+                print("Erro: O valor do depósito deve ser maior que zero.")
         else:
-            print(f"Erro: Sua conta está desativada.")
+            print("Erro: Sua conta está desativada.")
 
     def sacar(self, saque):
         if self.status_conta:
             if saque > 0:
-                if saque <= self.saldo:
-                    self.saldo -= saque
-                    print(f"Saque de R$ {saque} realizado com sucesso.")
+                if saque <= self.saldo + self.limite_disponivel:
+                    if saque > self.saldo:
+                        escolha = input("Saldo indisponível. Gostaria de utilizar o limite (s/n)? ")
+                        while escolha not in "ssSSnnNN":
+                            escolha = input("Resposta inválida. Digite novamente (s/n): ")
+                        if escolha in "sS":
+                            self.limite_disponivel -= (saque - self.saldo)
+                            self.saldo = 0
+                            print(f"Saque de R$ {saque:.2f} realizado com sucesso.")
+                        else:
+                            print(f"Erro: Valor a sacar é maior do que o saldo disponível.")
+                    else:
+                        self.saldo -= saque
+                elif self.saldo == 0:
+                    print(f"Erro: Seu saldo está zerado, e o saque ultrapassa o limite da conta disponível.")
                 else:
-                    print(f"Erro: Valor a sacar é maior do que o saldo disponível.")
-            elif saque == 0:
-                print(f"Erro: Digite um valor maior do que zero para sacar.")
+                    print(f"Erro: Saque ultrapassa o saldo e o limite da conta.")
             else:
-                print(f"Erro: Não é possível realizar um saque de valor negativo.")
+                print("Erro: O valor do saque deve ser maior que zero.")
         else:
-            print(f"Erro: Sua conta está desativada.")
+            print("Erro: Sua conta está desativada.")
 
-    def verificar_saldo(self):
+    def verificar_saldo_e_limite(self):
         if self.status_conta:
-            print(f"Saldo disponível: R$ {self.saldo}")
+            print(f"* Saldo disponível: R$ {self.saldo:.2f}\n"
+                  f"* Limite disponível: R$ {self.limite_disponivel:.2f}")
         else:
-            print(f"Erro: Sua conta está desativada.")
+            print("Erro: Sua conta está desativada.")
 
     def ativar(self):
         if not self.status_conta:
             self.status_conta = True
-            print(f"Sua conta foi ativada.")
+            print("Sua conta foi ativada.")
         else:
-            print(f"Sua conta já está ativa.")
+            print("Sua conta já está ativada.")
 
     def desativar(self):
         if self.saldo == 0:
             self.status_conta = False
-            print(f"Sua conta foi desativada.")
+            print("Sua conta foi desativada.")
         elif not self.status_conta:
-            print(f"Sua conta já está desativada.")
-        elif self.saldo < 0:
-            print(f"Erro: Seu saldo está negativo (é necessário zerar o saldo).")
+            print("Sua conta já está desativada.")
         else:
-            print(f"Erro: Saldo disponível na conta (é necessário zerar o saldo).")
+            print("Erro: Seu saldo não está zerado.")
 
 
-p1 = ContaBancaria("1234-5678-9012-3456", "Conta Poupança", 0.0, True, "Gabriel C. C. Santos")
+p1 = ContaBancaria("Gabriel C. C. Santos", "1234-5678-9012-3456", "Conta Poupança")
+
+p1.ativar()
+p1.sacar(10)
+p1.verificar_saldo_e_limite()
+p1.depositar(20)
+p1.verificar_saldo_e_limite()
